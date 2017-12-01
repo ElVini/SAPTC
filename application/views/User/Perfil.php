608e7e8dc457290f6c23c508d1eb4fa30afd0b09
ElViny <32659764+ElVini@users.noreply.github.com>
2017-12-01 10:32:33 -0700

    Ayuda :'v

--- application/views/User/Perfil.php
+++ application/views/User/Perfil.php
@@ -1,7 +1,14 @@
 <?php $this->load->view('Helpers/User/header'); ?>
 
 <div class="container">
-    <img src="<?php echo base_url().$img; ?>" height="100px" width="100px" style="border-radius: 100%;" alt="Imagen de perfil">
+    <div class="row">
+        <div class="col-md-2">
+            <img src="<?php echo base_url().$img; ?>" height="150px" width="150px" style="border-radius: 100%;" alt="imagen de perfil">
+        </div>
+        <div class="col-md-10">
+            <h2><?= $nombre ?></h2>
+        </div>
+    </div>
 </div>
 
 <?php $this->load->view('User/Helpers/footer') ?>
