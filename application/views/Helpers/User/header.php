9577199a1cbe15c169ca35dc941b0f178b17c06e
ElViny <32659764+ElVini@users.noreply.github.com>
2017-12-01 10:19:28 -0700

    Cambio al header c:

--- application/views/Helpers/User/header.php
+++ application/views/Helpers/User/header.php
@@ -36,12 +36,7 @@
           <ul class="nav navbar-nav">
                <!--PARA LA OPCION DE IR AL INICIO-->
             <li><a href="<?php echo base_url(); ?>" style="color:white;"><span class="glyphicon glyphicon-home"></span> Inicio</a></li>
-<<<<<<< HEAD
             <li><a href="<?php echo base_url('index.php/User/perfil'); ?>" style="color:white;"><span class="glyphicon glyphicon-user"></span> Usuario</a></li>
-=======
-               <!--PARA LAS OPCIONES DE USUARIO-->
-            <li><a href="<?php echo base_url(); ?>" style="color:white;"><span class="glyphicon glyphicon-user"></span> Usuario</a></li>
->>>>>>> 14dd8e4139004b42537718bcc99c1df51fe945a1
 
 
    <!--PARA LA OPCION DE SERVICIO A ALUMNOS-->
